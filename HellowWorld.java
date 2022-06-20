9ff409c9e44be0b83a68f6f482c9b05d38b9d8bb
yannamsiva <46184940+yannamsiva@users.noreply.github.com>
2022-06-20 17:33:40 +0530

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -4,6 +4,8 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Welcome To Ojas");
+		System.out.println("Thx For Working Hard");
+		
 	}
 
 }
